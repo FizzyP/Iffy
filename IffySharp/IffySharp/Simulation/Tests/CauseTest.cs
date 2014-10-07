4dfe3c5a4303b34c8f90ecba6ff494f3c81a95a9
Fabrizio Polo <fab@orbotix.com>
2014-10-07 08:47:07 -0600

    Refactor/rename.

--- IffySharp/IffySharp/Simulation/Tests/CauseTest.cs
+++ IffySharp/IffySharp/Simulation/Tests/CauseTest.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace IffySharp.Causes.Tests
+namespace IffySharp.Tests
 {
     class CauseTest
     {
--- IffySharp/IffySharp/Test/IffyTest.cs
+++ IffySharp/IffySharp/Test/IffyTest.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-using IffySharp.Causes.Tests;
+using IffySharp.Tests;
 
 namespace IffySharp.Test
 {
