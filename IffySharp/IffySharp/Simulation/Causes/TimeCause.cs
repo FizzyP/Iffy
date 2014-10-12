995c2015cf6340ddd1c03f022c8dc37653133577
FizzyP <fab@orbotix.com>
2014-10-12 09:16:21 -0600

    Working on queueing events at times.

--- IffySharp/IffySharp/Simulation/Causes/TimeCause.cs
+++ IffySharp/IffySharp/Simulation/Causes/TimeCause.cs
@@ -18,6 +18,11 @@ namespace IffySharp.Simulation
 		{
 			DateTime time = Time;
 			time.AddSeconds(dtSeconds);
+
+			//	Clean up the queue
+			doQueuedCauses (time);
+
+			//	Update the time cause itself if anything is listening to it directly
 			Time = time;	//	trigger the cause
 		}
 
@@ -25,6 +30,42 @@ namespace IffySharp.Simulation
         : base(startTime)
         {
         }
+
+		//	
+		//	Event Queue
+		//
+
+		//	DateTime ticks are long
+		private SortedDictionary<long, List<Cause>> eventQueue = new SortedDictionary<long, List<Cause>>();
+
+		void queueCause(DateTime atTime, Cause cause)
+		{
+			long time = atTime.Ticks;
+			if (!eventQueue.ContainsKey (time)) {
+				eventQueue [time] = new List<Cause> ();
+			}
+
+			eventQueue [time].Add (cause);
+		}
+
+		void doQueuedCauses(DateTime untilTime)
+		{
+			long untilTicks = untilTime.Ticks;
+			foreach (KeyValuePair<long, List<Cause>> kv in eventQueue) {
+				if (kv.Key > untilTicks)
+					break;
+
+				//	Advance time to this point and invoke the cause
+				Time = new DateTime (kv.Key);
+
+				//	The causes in the queue don't listen to Time directly.
+				//	Instead they are marked dirty here.  If they are also !IsLazy then
+				//	this will cause them to "occurr".
+				foreach (Cause cause in kv.Value) {
+					cause.IsDirty = true;
+				}
+			}
+		}
 
     }
 }
