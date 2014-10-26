1cafc127e90ec796abf001267a3e0ccdbec320ca
FizzyP <fab@orbotix.com>
2014-10-26 16:53:29 -0600

    basic parsing is working.

--- IffySharp/IffySharp/Parser/Parser.cs
+++ IffySharp/IffySharp/Parser/Parser.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Linq;
+using System.Collections.Generic;
+
 using IffySharp.SubParser;
 using IffySharp.Simulation;
 using EeekSoft.Text;
@@ -8,7 +10,7 @@ namespace IffySharp.Parser
 {
 	public class Parser
 	{
-		public delegate void SymbolicationTask(object[] symbols);
+		public delegate void ParsingTask(object[] symbols, int count);
 
 		private SymbolicKnowledge knowledge;
 
@@ -20,12 +22,49 @@ namespace IffySharp.Parser
 		public void parse(string text)
 		{
 			StringSearchResult[] results = findWordsInString (scrubString(text));
+			var tokenMap = convertResultsToTokenMap (results);
+
+			traverseTokens (tokenMap, text.Length, new ParsingTask (dummyParsingTask));
 
 			// Write all results  
 			foreach(StringSearchResult r in results)
 			{
 				Console.WriteLine("Keyword='{0}', Index={1}", r.Keyword, r.Index);
+			}
+		}
 
+		private void traverseTokens(Dictionary<int, MatchedToken> tokenMap, int length, ParsingTask task)
+		{
+			object[] tokens = new object[length];
+			recursivelyTraverseTokens (tokenMap, tokenMap[0], length, tokens, 0, task);
+		}
+
+		private void recursivelyTraverseTokens(Dictionary<int, MatchedToken> tokenMap, MatchedToken currToken, int length, object[] tokens, int tokenIndex, ParsingTask task)
+		{
+			object[] assocs = currToken.associatedObjects;
+//			tokenIndex++;
+
+			//	Can we extend this any farther?
+			//	If not then we're at the end, so invoke the task on each way of copmleting the token list.
+			if (currToken.index + currToken.text.Length >= length) {
+				foreach (object obj in assocs) {
+					tokens [tokenIndex] = obj;
+					task (tokens, tokenIndex + 1);
+				}
+			} else {
+				//	Otherwise recurse until we hit the end
+				int nextIndex = currToken.index + currToken.text.Length + 1;	// + 1 for space
+				if (!tokenMap.ContainsKey(nextIndex)) {
+					//	No way of continuing
+					return;
+				}
+
+				var nextToken = tokenMap [nextIndex];
+
+				foreach (object obj in assocs) {
+					tokens [tokenIndex] = obj;
+					recursivelyTraverseTokens (tokenMap, nextToken, length, tokens, tokenIndex + 1, task);
+				}
 			}
 		}
 
@@ -36,11 +75,52 @@ namespace IffySharp.Parser
 			return searchAlg.FindAll(text);
 		}
 
+		private Dictionary<int, MatchedToken> convertResultsToTokenMap(StringSearchResult[] results)
+		{
+			var tokenMap = new Dictionary<int, MatchedToken> ();
+	
+			foreach (StringSearchResult result in results) {
+				if (knowledge.Associations.ContainsKey (result.Keyword)) {
+					var wordAssoc = knowledge.Associations [result.Keyword];
+
+					var tok = new MatchedToken ();
+					tok.text = result.Keyword;
+					tok.index = result.Index;
+					tok.associatedObjects = wordAssoc.ToArray<object>();
+
+					//	Store the token in the map
+					tokenMap [tok.index] = tok;
+
+				} else {
+					throw new ParsingException ("Matched word not associated with an object.");
+				}
+			}
+
+			return tokenMap;
+		}
+
 		private string scrubString(string text)
 		{
 			//	TODO elimniate repeat whitespace and apply ToLower
 			return text;
 		}
+	
+
+		// public delegate void ParsingTask(object[] symbols, int count);
+
+		private static void dummyParsingTask(object[] symbols, int count)
+		{
+			Console.WriteLine (count);
+		}
+
+
+
+		private class MatchedToken {
+			public string text;
+			public int index;
+			public object[] associatedObjects;
+		}
+
 	}
 }
 
